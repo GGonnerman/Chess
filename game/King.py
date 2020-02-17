cb33475f505c0e752cf439ff3c3cdab13dd284ff
Gaston <gaston95g@gmail.com>
2020-02-17 11:54:37 -0600

    began work on king potential moves

--- game/King.py
+++ game/King.py
@@ -11,4 +11,23 @@ class King(ChessPiece, SetMovement):
 		super(King, self).__init__("King", color, row, column, canvas)
 
 	def get_potential_moves(self, gameboard):
-		return self.check_position(gameboard, [0, 1], [1, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1])
+
+
+
+		direction_list = [0, 1], [1, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]
+		return self.check_position(gameboard, *direction_list)
+
+		for row in range(len(gameboard.piece_list)):
+			for column in range(len(gameboard.piece_list[0])):
+				if isinstance(gameboard.get_piece(row, column), King) and gameboard.get_piece(row, column).color != self.color:
+					king = gameboard.get_piece(row, column)
+
+		delta_row = self.row - king.row
+		delta_column = self.column - king.column
+
+		print(f"MINE, row:{king.row}, column:{king.column}")
+		print(f"KING, row:{self.row}, column:{self.column}")
+
+		if delta_row > 2 and delta_column: return self.check_position(gameboard, *direction_list)
+
+		return self.check_position(gameboard, *direction_list)
